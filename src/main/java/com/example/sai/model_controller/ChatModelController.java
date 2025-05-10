ee15886bc3b47ab0d5fd58400a331ae44437a1d0
吴建川 <15512109007@163.com>
2025-05-11 01:05:36 +0800

    feat: Streaming Responses; change model at run-time.

--- src/main/java/com/example/sai/model_controller/ChatModelController.java
+++ src/main/java/com/example/sai/model_controller/ChatModelController.java
@@ -1,9 +1,12 @@
 package com.example.sai.model_controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -22,13 +25,19 @@ class ChatModelController {
         this.chatClient = chatClientBuilder.build();
     }
 
-//    @GetMapping("/ai")
-//    String generation(String userInput) {
-//        return this.chatClient.prompt()
-//                .user(userInput)
-//                .call()
-//                .content();
-//    }
+    /**
+     * 流式输出问答结果
+     */
+    @GetMapping(path = "/ai/{userInput}", produces = "text/html;charset=UTF-8")
+    Flux<String> generation(@PathVariable String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                // 动态切换模型
+                .options(OllamaOptions.builder().model("qwen3:4b").build())
+//                .options(OllamaOptions.builder().model("gemma3:4b").build())
+                .stream()
+                .content();
+    }
 
 //    @GetMapping("/ai")
 //    ChatResponse generation(String userInput) {
@@ -38,19 +47,19 @@ class ChatModelController {
 //                .chatResponse();
 //    }
 
-    @GetMapping("/ai")
-    ActorFilms generation(String userInput) {
-        String structuredPrompt = """
-        请根据以下要求严格返回JSON数据：
-        1. 格式必须为：{"actor":"演员姓名", "movies":["电影1","电影2"]}
-        2. 不要包含任何额外描述或Schema
-        3. 用户输入：%s
-        """.formatted(userInput);
-
-        return this.chatClient.prompt()
-                .user(structuredPrompt)
-                .call()
-                .entity(ActorFilms.class);
-    }
-    record ActorFilms(String actor, List<String> movies) {}
+//    @GetMapping("/ai")
+//    ActorFilms generation(String userInput) {
+//        String structuredPrompt = """
+//        请根据以下要求严格返回JSON数据：
+//        1. 格式必须为：{"actor":"演员姓名", "movies":["电影1","电影2"]}
+//        2. 不要包含任何额外描述或Schema
+//        3. 用户输入：%s
+//        """.formatted(userInput);
+//
+//        return this.chatClient.prompt()
+//                .user(structuredPrompt)
+//                .call()
+//                .entity(ActorFilms.class);
+//    }
+//    record ActorFilms(String actor, List<String> movies) {}
 }
