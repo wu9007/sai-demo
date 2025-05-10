4375258a1d70191e6ba792936a4d5e90acf061a3
吴建川 <15512109007@163.com>
2025-05-10 21:12:29 +0800

    feat: Add the documents to PGVector.

--- src/main/java/com/example/sai/rag_controller/RagController.java
+++ src/main/java/com/example/sai/rag_controller/RagController.java
@@ -1,7 +1,16 @@
 package com.example.sai.rag_controller;
 
 import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.filter.Filter;
+import org.springframework.ai.vectorstore.filter.FilterExpressionBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,28 +25,56 @@ import java.util.List;
 @RequestMapping("/rag")
 public class RagController {
 
-    private final MyPagePdfDocumentReader reader;
-    private final MyTokenTextSplitter tokenTextSplitter;
+    private final VectorStore vectorStore;
 
-    public RagController(MyPagePdfDocumentReader reader, MyTokenTextSplitter tokenTextSplitter) {
-        this.reader = reader;
-        this.tokenTextSplitter = tokenTextSplitter;
+    public RagController(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
     }
 
     @GetMapping("/etl")
     public String etl() {
         //读取文档
-        List<Document> docsFromPdfWithCatalog = reader.getDocsFromPdf();
+        // TODO 改为由用户上传
+        PagePdfDocumentReader pdfReader = new PagePdfDocumentReader("classpath:/sample1.pdf",
+                PdfDocumentReaderConfig.builder()
+                        .withPageTopMargin(0)
+                        .withPageExtractedTextFormatter(ExtractedTextFormatter.builder()
+                                .withNumberOfTopTextLinesToDelete(0)
+                                .build())
+                        .withPagesPerDocument(1)
+                        .build());
+
+        List<Document> docsFromPdfWithCatalog = pdfReader.read();
+
         //转换
-        List<Document> splitDocuments = tokenTextSplitter.splitDocuments(docsFromPdfWithCatalog);
+        TokenTextSplitter splitter = new TokenTextSplitter();
+        List<Document> splitDocuments = splitter.apply(docsFromPdfWithCatalog);
         for (Document doc : splitDocuments) {
             System.out.println("Chunk: " + doc.getFormattedContent());
             System.out.println("Metadata: " + doc.getMetadata());
+            doc.getMetadata().put("author", "9007");
         }
 
-        //TODO https://docs.spring.io/spring-ai/reference/api/etl-pipeline.html
-        //TODO 写入向量库
+        // Add the documents to PGVector
+        vectorStore.add(splitDocuments);
         return "success";
+    }
+
+    @GetMapping("/query/{text}")
+    public List<Document> etl(@PathVariable String text) {
+        // Retrieve documents similar to a query
+        FilterExpressionBuilder filterExpressionBuilder = new FilterExpressionBuilder();
+        Filter.Expression expression = filterExpressionBuilder.and(
+                        filterExpressionBuilder.eq("file_name", "sample1.pdf"),
+                        filterExpressionBuilder.gt("page_number", 1)
+                )
+                .build();
+        return this.vectorStore.similaritySearch(SearchRequest.builder()
+                .query(text)
+                .topK(5)
+                .similarityThreshold(0.5)
+                .filterExpression(expression)
+                .build());
     }
 
 }
